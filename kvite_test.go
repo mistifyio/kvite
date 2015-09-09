678fe8ce8fcf2c0943f025b9be80bc4c12730c44
Brian Bickerton <brian.bickerton@gmail.com>
2015-09-10 07:23:03 +1000

    Convert to Testify and do some basic test cleanup

--- kvite_test.go
+++ kvite_test.go
@@ -1,318 +1,181 @@
 package kvite
 
 import (
-	"fmt"
 	"io/ioutil"
 	"os"
 	"path/filepath"
-	"reflect"
-	"runtime"
 	"testing"
+
+	log "github.com/Sirupsen/logrus"
+	logx "github.com/mistifyio/mistify-logrus-ext"
+	"github.com/stretchr/testify/suite"
 )
 
-func withDB(t *testing.T, fn func(db *DB, t *testing.T)) {
-	file := tempfile()
-	db, err := Open(file, "testing")
-	ok(t, err)
-	defer removeFileAndLogError(file)
-	defer logErr(db.Close, "database close")
-	fn(db, t)
+type KViteTestSuite struct {
+	suite.Suite
+	DB      *DB
+	TempDir string
 }
 
-func TestOpen(t *testing.T) {
-	withDB(t, func(db *DB, t *testing.T) {
+func (s *KViteTestSuite) SetupTest() {
+	// Create a new db tempdir
+	dir, err := ioutil.TempDir("", "kvite-")
+	s.NoError(err)
+	s.TempDir = dir
+
+	// Open the db with the tempdir
+	db, err := Open(filepath.Join(s.TempDir, "kvite.db"), "testing")
+	s.NoError(err)
+	s.DB = db
+}
+
+func (s *KViteTestSuite) TearDownTest() {
+	// Close the db
+	logx.LogReturnedErr(s.DB.Close, nil, "failed to close database")
+
+	// Clean up the db tempdir
+	logx.LogReturnedErr(func() error { return os.RemoveAll(s.TempDir) },
+		log.Fields{"tempdir": s.TempDir}, "failed to remove tempdir")
+}
+
+func TestKViteTestSuite(t *testing.T) {
+	suite.Run(t, new(KViteTestSuite))
+}
+
+func (s *KViteTestSuite) TestDBOpen() {
+	// The suite test setup tests a good call to the kvite.Open function
+	s.Equal("testing", s.DB.table)
+}
+
+func (s *KViteTestSuite) TestDBClose() {
+	// The suite test setup tests a good call to the kvite.Close function
+	// Attempt to close again
+	s.NoError(s.DB.Close())
+}
+
+func (s *KViteTestSuite) TestDBBegin() {
+	tx, err := s.DB.Begin()
+	s.NoError(err)
+	s.NotNil(tx)
+}
+
+func (s *KViteTestSuite) TestTXRollback() {
+	tx, _ := s.DB.Begin()
+	s.NoError(tx.Rollback())
+	s.Error(tx.Rollback())
+}
+
+func (s *KViteTestSuite) TestTXCommit() {
+	tx, _ := s.DB.Begin()
+	s.NoError(tx.Commit())
+	s.NoError(tx.Commit())
+}
+
+func (s *KViteTestSuite) TestCreateBucket() {
+	tx, _ := s.DB.Begin()
+	b, err := tx.CreateBucket("test")
+	s.NoError(err)
+	s.NotNil(b)
+	s.NoError(tx.Commit())
+}
+
+func (s *KViteTestSuite) TestCreateBucketIfNotExists() {
+	tx, _ := s.DB.Begin()
+	b, err := tx.CreateBucketIfNotExists("test")
+	s.NoError(err)
+	s.NotNil(b)
+	s.NoError(tx.Commit())
+}
+
+func (s *KViteTestSuite) TestPut() {
+	tx, _ := s.DB.Begin()
+	b, _ := tx.CreateBucket("test")
+
+	s.NoError(b.Put("foo", []byte("bar")))
+	s.NoError(tx.Commit())
+}
+
+func (s *KViteTestSuite) TestGet() {
+	tx, err := s.DB.Begin()
+	b, err := tx.CreateBucket("test")
+
+	_ = b.Put("foo", []byte("bar"))
+
+	val, err := b.Get("foo")
+	s.NoError(err)
+	s.Equal("bar", string(val))
+}
+
+func (s *KViteTestSuite) TestDelete() {
+	tx, _ := s.DB.Begin()
+	b, _ := tx.CreateBucket("test")
+	_ = b.Put("foo", []byte("bar"))
+
+	s.NoError(b.Delete("foo"))
+
+	val, _ := b.Get("foo")
+	s.Equal([]byte(nil), val)
+
+	s.NoError(tx.Commit())
+}
+
+func (s *KViteTestSuite) TestTransaction() {
+	err := s.DB.Transaction(func(tx *Tx) error {
+		b, _ := tx.CreateBucket("test")
+		_ = b.Put("foo", []byte("bar"))
+		return nil
 	})
+	s.NoError(err)
+
+	tx, _ := s.DB.Begin()
+	b, _ := tx.CreateBucket("test")
+	val, _ := b.Get("foo")
+	s.Equal(string(val), "bar")
 }
 
-func TestBegin(t *testing.T) {
-	withDB(t, func(db *DB, t *testing.T) {
-		tx, err := db.Begin()
-		if err != nil {
-			t.Fatal(err)
-		}
-		defer logErr(tx.Rollback, "Transaction Rollback")
+func (s *KViteTestSuite) TestForEach() {
+	tx, _ := s.DB.Begin()
+	b, _ := tx.CreateBucket("test")
+
+	_ = b.Put("foo", []byte("bar"))
+	_ = b.Put("baz", []byte("stuff"))
+
+	var items []string
+	err := b.ForEach(func(k string, v []byte) error {
+		items = append(items, k)
+		return nil
 	})
+	s.NoError(err)
+	s.Len(items, 2)
 }
 
-func TestRollback(t *testing.T) {
-	withDB(t, func(db *DB, t *testing.T) {
-		tx, err := db.Begin()
-		ok(t, err)
-		err = tx.Rollback()
-		ok(t, err)
-	})
-}
-
-func TestCommit(t *testing.T) {
-	withDB(t, func(db *DB, t *testing.T) {
-		tx, err := db.Begin()
-		ok(t, err)
-		err = tx.Commit()
-		ok(t, err)
-	})
-}
-
-func TestCreateBucket(t *testing.T) {
-	withDB(t, func(db *DB, t *testing.T) {
-		tx, err := db.Begin()
-		ok(t, err)
-		defer logErr(tx.Rollback, "Transaction Rollback")
-		_, err = tx.CreateBucket("test")
-		ok(t, err)
-		err = tx.Commit()
-		ok(t, err)
-	})
-}
-
-func TestCreateBucketIfNotExists(t *testing.T) {
-	withDB(t, func(db *DB, t *testing.T) {
-		tx, err := db.Begin()
-		ok(t, err)
-		defer logErr(tx.Rollback, "Transaction Rollback")
-		_, err = tx.CreateBucketIfNotExists("test")
-		ok(t, err)
-		err = tx.Commit()
-		ok(t, err)
-	})
-}
-
-func TestPut(t *testing.T) {
-	withDB(t, func(db *DB, t *testing.T) {
-		tx, err := db.Begin()
-		ok(t, err)
-		defer logErr(tx.Rollback, "Transaction Rollback")
-		b, err := tx.CreateBucket("test")
-		ok(t, err)
-
-		err = b.Put("foo", []byte("bar"))
-		ok(t, err)
-		err = tx.Commit()
-		ok(t, err)
-	})
-}
-
-func TestGet(t *testing.T) {
-	withDB(t, func(db *DB, t *testing.T) {
-		tx, err := db.Begin()
-		ok(t, err)
-		defer logErr(tx.Rollback, "Transaction Rollback")
-		b, err := tx.CreateBucket("test")
-		ok(t, err)
-
-		err = b.Put("foo", []byte("bar"))
-		ok(t, err)
-
-		val, err := b.Get("foo")
-		ok(t, err)
-
-		equals(t, string(val), "bar")
-
-		err = tx.Commit()
-		ok(t, err)
-	})
-}
-
-func TestDelete(t *testing.T) {
-	withDB(t, func(db *DB, t *testing.T) {
-		tx, err := db.Begin()
-		ok(t, err)
-		defer logErr(tx.Rollback, "Transaction Rollback")
-		b, err := tx.CreateBucket("test")
-		ok(t, err)
-
-		err = b.Put("foo", []byte("bar"))
-		ok(t, err)
-
-		val, err := b.Get("foo")
-		ok(t, err)
-
-		equals(t, string(val), "bar")
-
-		err = b.Delete("foo")
-		ok(t, err)
-
-		val, err = b.Get("foo")
-		equals(t, []byte(nil), val)
-		ok(t, err)
-
-		err = tx.Commit()
-		ok(t, err)
-	})
-}
-
-func TestTransaction(t *testing.T) {
-	withDB(t, func(db *DB, t *testing.T) {
-		err := db.Transaction(func(tx *Tx) error {
-			b, err := tx.CreateBucket("test")
-			ok(t, err)
-
-			err = b.Put("foo", []byte("bar"))
-			ok(t, err)
-
-			val, err := b.Get("foo")
-			ok(t, err)
-
-			equals(t, string(val), "bar")
-
-			return nil
-		})
-
-		if err != nil {
-			t.Fatal(err)
-		}
-	})
-}
-
-func TestForEach(t *testing.T) {
-	withDB(t, func(db *DB, t *testing.T) {
-		err := db.Transaction(func(tx *Tx) error {
-			b, err := tx.CreateBucket("test")
-			ok(t, err)
-
-			err = b.Put("foo", []byte("bar"))
-			ok(t, err)
-
-			err = b.Put("baz", []byte("stuff"))
-			ok(t, err)
-
-			var items []string
-			err = b.ForEach(func(k string, v []byte) error {
-				items = append(items, k)
-				return nil
-			})
-			ok(t, err)
-
-			if len(items) != 2 {
-				return fmt.Errorf("length does not match")
-			}
-
-			return nil
-		})
-
-		ok(t, err)
-	})
-}
-
-func TestBuckets(t *testing.T) {
+func (s *KViteTestSuite) TestBuckets() {
 	buckets := []string{"one", "two", "three"}
-	withDB(t, func(db *DB, t *testing.T) {
-		err := db.Transaction(func(tx *Tx) error {
-			for _, name := range buckets {
-				b, err := tx.CreateBucket(name)
-				ok(t, err)
-				err = b.Put("foo", []byte("bar"))
-				ok(t, err)
-			}
-			return nil
-		})
-
-		names, err := db.Buckets()
-		ok(t, err)
-		equals(t, buckets, names)
-	})
-}
-
-func TestUnique(t *testing.T) {
-	withDB(t, func(db *DB, t *testing.T) {
-		err := db.Transaction(func(tx *Tx) error {
-			b, err := tx.CreateBucket("test")
-			ok(t, err)
-
-			err = b.Put("foo", []byte("bar"))
-			ok(t, err)
-			err = b.Put("foo", []byte("baz"))
-
-			err = b.ForEach(func(k string, v []byte) error {
-				equals(t, "baz", string(v))
-				return nil
-			})
-			ok(t, err)
-			return nil
-		})
-		ok(t, err)
-	})
-}
-
-func BenchmarkPutGet(bm *testing.B) {
-	file := tempfile()
-	db, err := Open(file, "testing")
-
-	if err != nil {
-		bm.Fatal(err)
-	}
-	defer removeFileAndLogError(file)
-	defer logErr(db.Close, "database close")
-
-	err = db.Transaction(func(tx *Tx) error {
-		b, err := tx.CreateBucket("test")
-		if err != nil {
-			return err
-		}
-
-		for n := 0; n < bm.N; n++ {
-			err = b.Put("foo", []byte("bar"))
-			if err != nil {
-				return err
-			}
-			_, err := b.Get("foo")
-			if err != nil {
-				return err
-			}
+	_ = s.DB.Transaction(func(tx *Tx) error {
+		for _, name := range buckets {
+			b, _ := tx.CreateBucket(name)
+			_ = b.Put("foo", []byte("bar"))
 		}
 		return nil
 	})
-	if err != nil {
-		bm.Fatal(err)
-	}
+
+	names, err := s.DB.Buckets()
+	s.NoError(err)
+	s.Equal(buckets, names)
 }
 
-// tempfile returns a temporary file path.
-func tempfile() string {
-	f, _ := ioutil.TempFile("", "kvite-")
-	logErr(f.Close, "temp file close")
-	removeFileAndLogError(f.Name())
-	return f.Name()
-}
+func (s *KViteTestSuite) TestUnique() {
+	tx, _ := s.DB.Begin()
+	b, _ := tx.CreateBucket("test")
 
-// Thanks to https://github.com/benbjohnson/testing
+	s.NoError(b.Put("foo", []byte("bar")))
+	s.NoError(b.Put("foo", []byte("baz")))
 
-// assert fails the test if the condition is false.
-func assert(tb testing.TB, condition bool, msg string, v ...interface{}) {
-	if !condition {
-		_, file, line, _ := runtime.Caller(1)
-		fmt.Printf("\033[31m%s:%d: "+msg+"\033[39m\n\n", append([]interface{}{filepath.Base(file), line}, v...)...)
-		tb.FailNow()
-	}
-}
-
-// ok fails the test if an err is not nil.
-func ok(tb testing.TB, err error) {
-	if err != nil {
-		_, file, line, _ := runtime.Caller(1)
-		fmt.Printf("\033[31m%s:%d: unexpected error: %s\033[39m\n\n", filepath.Base(file), line, err.Error())
-		tb.FailNow()
-	}
-}
-
-// equals fails the test if exp is not equal to act.
-func equals(tb testing.TB, exp, act interface{}) {
-	if !reflect.DeepEqual(exp, act) {
-		_, file, line, _ := runtime.Caller(1)
-		fmt.Printf("\033[31m%s:%d:\n\n\texp: %#v\n\n\tgot: %#v\033[39m\n\n", filepath.Base(file), line, exp, act)
-		tb.FailNow()
-	}
-}
-
-func logErr(fn func() error, message string) {
-	if err := fn(); err != nil {
-		_, file, line, _ := runtime.Caller(1)
-		fmt.Printf("%s:%d: Error: %s: %s", filepath.Base(file), line, message, err.Error())
-	}
-}
-
-func removeFileAndLogError(file string) {
-	if err := os.Remove(file); err != nil {
-		_, callerFile, line, _ := runtime.Caller(1)
-		fmt.Printf("%s:%d: Error removing file '%s': %s", filepath.Base(callerFile), line, file, err.Error())
-	}
+	i := 0
+	_ = b.ForEach(func(k string, v []byte) error {
+		i++
+		s.Equal("baz", string(v))
+		return nil
+	})
+	s.Equal(1, i)
 }
