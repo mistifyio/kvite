544d4df9284660b924d0dcc7b17d5db151859f44
Brian Bickerton <brian.bickerton@gmail.com>
2015-09-11 02:01:31 +1000

    Increase code coverage and clean up tests

--- kvite_test.go
+++ kvite_test.go
@@ -1,6 +1,7 @@
 package kvite
 
 import (
+	"errors"
 	"io/ioutil"
 	"os"
 	"path/filepath"
@@ -38,13 +39,41 @@ func (s *KViteTestSuite) TearDownTest() {
 		log.Fields{"tempdir": s.TempDir}, "failed to remove tempdir")
 }
 
+func (s *KViteTestSuite) testStoredValue(bucketName, key string, expectedValue []byte) {
+	tx, _ := s.DB.Begin()
+	b, _ := tx.CreateBucket(bucketName)
+	value, getErr := b.Get(key)
+	s.NoError(getErr)
+	s.Equal(expectedValue, value)
+	_ = tx.Rollback()
+}
+
 func TestKViteTestSuite(t *testing.T) {
 	suite.Run(t, new(KViteTestSuite))
 }
 
 func (s *KViteTestSuite) TestDBOpen() {
 	// The suite test setup tests a good call to the kvite.Open function
-	s.Equal("testing", s.DB.table)
+
+	tests := []struct {
+		filename    string
+		table       string
+		expectedErr bool
+		msg         string
+	}{
+		{"", "", true, "directory as db file"},
+		{"open-test-bad-table.db", "1-23aa'1234", true, "invalid table name"},
+		{"open-test-no-table.db", "", false, "no supplied table name"},
+	}
+
+	for _, test := range tests {
+		_, err := Open(filepath.Join(s.TempDir, test.filename), test.table)
+		if test.expectedErr {
+			s.Error(err, test.msg)
+		} else {
+			s.NoError(err, test.msg)
+		}
+	}
 }
 
 func (s *KViteTestSuite) TestDBClose() {
@@ -59,19 +88,21 @@ func (s *KViteTestSuite) TestDBBegin() {
 	s.NotNil(tx)
 }
 
-func (s *KViteTestSuite) TestTXRollback() {
+func (s *KViteTestSuite) TestTxRollback() {
 	tx, _ := s.DB.Begin()
 	s.NoError(tx.Rollback())
+	// Can't rollback a finished tx
 	s.Error(tx.Rollback())
 }
 
-func (s *KViteTestSuite) TestTXCommit() {
+func (s *KViteTestSuite) TestTxCommit() {
 	tx, _ := s.DB.Begin()
 	s.NoError(tx.Commit())
-	s.NoError(tx.Commit())
+	// Can't commit a finished tx
+	s.Error(tx.Commit())
 }
 
-func (s *KViteTestSuite) TestCreateBucket() {
+func (s *KViteTestSuite) TestTxCreateBucket() {
 	tx, _ := s.DB.Begin()
 	b, err := tx.CreateBucket("test")
 	s.NoError(err)
@@ -79,7 +110,7 @@ func (s *KViteTestSuite) TestCreateBucket() {
 	s.NoError(tx.Commit())
 }
 
-func (s *KViteTestSuite) TestCreateBucketIfNotExists() {
+func (s *KViteTestSuite) TestTxCreateBucketIfNotExists() {
 	tx, _ := s.DB.Begin()
 	b, err := tx.CreateBucketIfNotExists("test")
 	s.NoError(err)
@@ -87,7 +118,7 @@ func (s *KViteTestSuite) TestCreateBucketIfNotExists() {
 	s.NoError(tx.Commit())
 }
 
-func (s *KViteTestSuite) TestPut() {
+func (s *KViteTestSuite) TestBucketPut() {
 	tx, _ := s.DB.Begin()
 	b, _ := tx.CreateBucket("test")
 
@@ -95,51 +126,102 @@ func (s *KViteTestSuite) TestPut() {
 	s.NoError(tx.Commit())
 }
 
-func (s *KViteTestSuite) TestGet() {
+func (s *KViteTestSuite) TestBucketGet() {
+	bucketName := "test"
+	key := "foo"
+	value := []byte("bar")
+
 	tx, err := s.DB.Begin()
-	b, err := tx.CreateBucket("test")
+	b, err := tx.CreateBucket(bucketName)
 
-	_ = b.Put("foo", []byte("bar"))
+	_ = b.Put(key, value)
 
-	val, err := b.Get("foo")
+	// Get value inside tx
+	val, err := b.Get(key)
 	s.NoError(err)
-	s.Equal("bar", string(val))
+	s.EqualValues(value, val)
+
+	s.NoError(tx.Commit())
+
+	// Get value after tx
+	s.testStoredValue(bucketName, key, value)
+	// Get non-existent value
+	s.testStoredValue(bucketName, "asdf", []byte(nil))
 }
 
-func (s *KViteTestSuite) TestDelete() {
+func (s *KViteTestSuite) TestBucketDelete() {
+	bucketName := "test"
+	key := "foo"
+	value := []byte("bar")
+
+	// Put and remove in same tx
 	tx, _ := s.DB.Begin()
-	b, _ := tx.CreateBucket("test")
-	_ = b.Put("foo", []byte("bar"))
+	b, _ := tx.CreateBucket(bucketName)
+	_ = b.Put(key, value)
+	s.NoError(b.Delete(key))
 
-	s.NoError(b.Delete("foo"))
-
-	val, _ := b.Get("foo")
+	// Check removed in tx
+	val, _ := b.Get(key)
 	s.Equal([]byte(nil), val)
 
 	s.NoError(tx.Commit())
+
+	// Check removed after tx
+	s.testStoredValue(bucketName, key, []byte(nil))
+
+	// Put and remove in different tx
+	tx, _ = s.DB.Begin()
+	b, _ = tx.CreateBucket(bucketName)
+	_ = b.Put(key, value)
+	tx.Commit()
+
+	tx, _ = s.DB.Begin()
+	b, _ = tx.CreateBucket(bucketName)
+	s.NoError(b.Delete(key))
+	s.NoError(tx.Commit())
+
+	// Check removed after tx
+	s.testStoredValue(bucketName, key, []byte(nil))
 }
 
-func (s *KViteTestSuite) TestTransaction() {
+func (s *KViteTestSuite) TestDBTransaction() {
+	bucketName := "test"
+	key := "foo"
+	value := []byte("bar")
+
+	// No error, tx should commit
 	err := s.DB.Transaction(func(tx *Tx) error {
-		b, _ := tx.CreateBucket("test")
-		_ = b.Put("foo", []byte("bar"))
+		b, _ := tx.CreateBucket(bucketName)
+		_ = b.Put(key, value)
 		return nil
 	})
 	s.NoError(err)
+	s.testStoredValue(bucketName, key, value)
 
-	tx, _ := s.DB.Begin()
-	b, _ := tx.CreateBucket("test")
-	val, _ := b.Get("foo")
-	s.Equal(string(val), "bar")
+	// Error, tx should rollback
+	err = s.DB.Transaction(func(tx *Tx) error {
+		b, _ := tx.CreateBucket(bucketName)
+		_ = b.Put(key, []byte("asdf"))
+
+		// Can't commit or rollback inside a db managed tx
+		s.Error(tx.Commit())
+		s.Error(tx.Rollback())
+
+		return errors.New("an error")
+	})
+	s.Error(err)
+	// Still original set value
+	s.testStoredValue(bucketName, key, value)
 }
 
-func (s *KViteTestSuite) TestForEach() {
+func (s *KViteTestSuite) TestBucketForEach() {
 	tx, _ := s.DB.Begin()
 	b, _ := tx.CreateBucket("test")
 
 	_ = b.Put("foo", []byte("bar"))
 	_ = b.Put("baz", []byte("stuff"))
 
+	// No error in fn
 	var items []string
 	err := b.ForEach(func(k string, v []byte) error {
 		items = append(items, k)
@@ -147,9 +229,16 @@ func (s *KViteTestSuite) TestForEach() {
 	})
 	s.NoError(err)
 	s.Len(items, 2)
+
+	// Error in fn
+	err = b.ForEach(func(k string, v []byte) error {
+		return errors.New("an error")
+	})
+	s.Error(err)
+
 }
 
-func (s *KViteTestSuite) TestBuckets() {
+func (s *KViteTestSuite) TestDBBuckets() {
 	buckets := []string{"one", "two", "three"}
 	_ = s.DB.Transaction(func(tx *Tx) error {
 		for _, name := range buckets {
@@ -164,17 +253,22 @@ func (s *KViteTestSuite) TestBuckets() {
 	s.Equal(buckets, names)
 }
 
-func (s *KViteTestSuite) TestUnique() {
-	tx, _ := s.DB.Begin()
-	b, _ := tx.CreateBucket("test")
+func (s *KViteTestSuite) TestBucketPutUnique() {
+	bucketName := "test"
+	key := "foo"
+	value := []byte("bar")
 
-	s.NoError(b.Put("foo", []byte("bar")))
-	s.NoError(b.Put("foo", []byte("baz")))
+	tx, _ := s.DB.Begin()
+	b, _ := tx.CreateBucket(bucketName)
+
+	// Should only be one key set and should be last set value
+	s.NoError(b.Put(key, []byte("baz")))
+	s.NoError(b.Put(key, value))
 
 	i := 0
 	_ = b.ForEach(func(k string, v []byte) error {
 		i++
-		s.Equal("baz", string(v))
+		s.Equal(value, v)
 		return nil
 	})
 	s.Equal(1, i)
